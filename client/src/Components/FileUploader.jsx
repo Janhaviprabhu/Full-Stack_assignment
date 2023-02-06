cd5973f7e294e92992ed7cb24f79f5c679537ce1
Janhaviprabhu <januprabhu26@gmail.com>
2023-02-06 16:39:59 +0530

    Front end added

--- client/src/Components/FileUploader.jsx
+++ client/src/Components/FileUploader.jsx
@@ -1,21 +1,68 @@
-import React, { useState } from 'react';
-import { Button, Input } from '@material-ui/core';
+import React, { useState } from "react";
+import { Button, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-function FileUpload() {
-  const [file, setFile] = useState(null);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
-  const handleChange = (event) => {
-    setFile(event.target.files[0]);
+const FileUplaod = () => {
+  const classes = useStyles();
+  const [videos, setVideos] = useState([]);
+  const [jsonObjects, setJsonObjects] = useState([]);
+  const [files, setFiles] = useState([]);
+const [uploadedFiles, setUploadedFiles] = useState([]);
+  
+  const handleFileUpload = (event, setFile) => {
+    setFile(event.target.files);
   };
+const onSubmit = async (e) => {
+  e.preventDefault()
+  for (const file of files) {
+    const uploadedFile = await uploadedFile(file);
+    setUploadedFiles([...uploadedFiles, uploadedFile]);
+  }
+};
 
   return (
-    <div>
-      <Input type="file" onChange={handleChange} />
-      <Button variant="contained" color="primary">
-        Upload
-      </Button>
-    </div>
+    <Grid container direction="column" alignItems="center">
+      <Typography variant="h5">Upload Screen</Typography>
+      <form className={classes.root} noValidate autoComplete="off">
+        <input
+          accept="video/*"
+          style={{ display: 'none' }}
+          id="contained-button-file-video"
+          multiple
+          type="file"
+          onChange={(event) => handleFileUpload(event, setVideos)}
+        />
+        <label htmlFor="contained-button-file-video">
+          <Button onClick={onSubmit} variant="contained" color="primary" component="span">
+            Upload Videos
+          </Button>
+        </label>
+        <br />
+        <input
+          accept=".json"
+          style={{ display: 'none' }}
+          id="contained-button-file-json"
+          multiple
+          type="file"
+          onChange={(event) => handleFileUpload(event, setJsonObjects)}
+        />
+        <label htmlFor="contained-button-file-json">
+          <Button onClick={onSubmit} variant="contained" color="primary" component="span">
+            Upload JSON Objects
+          </Button>
+        </label>
+      </form>
+    </Grid>
   );
-}
+};
 
-export default FileUpload;
+export default FileUplaod;
